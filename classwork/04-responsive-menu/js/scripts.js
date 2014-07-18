8830160346f6aac4f3d82ed7fccb733a816be37d
erictweigle <erictweigle@gmail.com>
2014-07-18 00:52:49 +0000

    convert hover to click on responsive menu

---
+++ classwork/04-responsive-menu/js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  var ww = document.body.clientwidth;
+  
+  $(".nav li a").each(function(){
+    if($(this).next().length>0){
+      $(this).addClass("parent");
+    }
+  });
+  
+  if(ww < 800){
+    $(".toggleMenu").css("display","inline-block");
+    $(".nav li a").click(function(){
+      $(this).parent("li").toggleClass("hover");
+    });
+  }else{
+    $(".toggleMenu").css("display","none");
+    $(".nav li").hover(function(){
+      $(this).addClass("hover");
+    }, function() {
+      $(this).removeClass("hover");
+    });
+  }
+});
